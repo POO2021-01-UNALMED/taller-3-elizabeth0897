5f3bbae77d1b0d71c337fe57b73828ff1844ee66
Elizabeth <elramirezo@unal.edu.co>
2021-03-16 23:09:28 -0500

    tarea

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,89 @@
+package taller3.televisores;
+
+public class TV {
+	
+	private Marca marca;
+	private int canal = 1;
+	private int precio = 500;
+	private int volumen = 1;
+	private boolean estado;
+	Control control;
+	private static int numTV;
+	
+	public TV(Marca marca,boolean estado) { //constructor
+		this.marca = marca;
+		this.estado = estado;
+	}
+	
+	public void setMarca(Marca marca) {
+		this.marca = marca;	
+	}	
+	public void setControl(Control control) {
+		this.control = control;	
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;	
+	}
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;	
+	}
+	public void setCanal(int canal) {
+		this.canal = canal;	
+	}
+	
+	public Marca getMarca() {
+		return marca;
+	}
+	public Control getControl() {
+		return control;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public int getVolumen() {
+		return volumen;
+	}
+	public int getCanal() {
+		return canal;
+	}
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	public static int numeroTelevisores() {
+		return numTV;
+	}
+	
+	public void turnOn() {
+		estado = true;
+	}
+	
+	public void turnOff() {
+		estado = false;
+	}
+	
+	public void canalUp() {
+		if(this.estado == true && 1 <= canal && canal < 120){
+			canal += 1;			
+		}
+	}
+	
+	public void canalDown() {
+		if(this.estado == true && 1 < canal && canal <= 120){
+			canal -= 1;
+		}
+		
+	}
+	
+	public void volumenUp() {
+		if(this.estado == true && 0 <= volumen && canal < 7){
+			volumen += 1;
+		}		
+	}
+	
+	public void volumenDown() {
+		if(this.estado == true && 0 < volumen && canal <= 7){
+			volumen -= 1;
+		}
+	}
+}
